22864f80986351a97ef0acdb4d64f92e20b4c525
gayoung <did-619@naver.com>
2022-10-11 13:07:26 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+ $('#cnt2 .swiper-button-prev').on('click', function () {
+   $(this)
+ }); 
+ $('#cnt2 .swiper-button-next').on('click', function () {
+
+ }); 
+ 
+});
+
+
